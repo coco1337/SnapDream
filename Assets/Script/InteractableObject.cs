18dc0ff105d92826906a227df51c111e0bf1aebc
coco1337 <scar3517@gmail.com>
2020-04-17 22:53:25 +0900

    Add InteractableObject.cs

---
+++ Assets/Script/InteractableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    // 기본적으로 밀기는 가능, 들수 있는지만 확인
+    public bool isHoldableObject;
+}
