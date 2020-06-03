34df2db1296031504319f33d199f0198129178a6
coco1337 <scar3517@gmail.com>
2020-06-03 16:33:19 +0900

    Add SendMailToDev
    피드백, 문의사항 전송용 스크립트 작성

---
+++ Assets/Script/SendMailToDev.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SendMailToDev : MonoBehaviour
+{
+    public Text textObj;
+
+    public void OnClickSend()
+    {
+        StartCoroutine(Send());
+    }
+    
+    IEnumerator Send()
+    {
+        var formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection(textObj.text));
+        UnityWebRequest www = UnityWebRequest.Post("https://coco1337.asuscomm.com/feedback", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            // TODO : 실패
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // TODO : Send 완료 메시지 연결
+            Debug.Log("Send Complete");
+        }
+    }
+}
