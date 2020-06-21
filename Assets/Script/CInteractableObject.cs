8292867d9fb186df5c3539a974048f343b5ccc9c
coco1337 <scar3517@gmail.com>
2020-06-22 00:15:50 +0900

    edit Physics for interactableObjects
    edit GroundCheck, gravity, drag, hit wall

---
+++ Assets/Script/CInteractableObject.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CInteractableObject : MonoBehaviour
+{
+    [SerializeField] protected bool isGround;
+
+    [Header("Physics")] 
+    [SerializeField] protected Vector2 moveDirection;
+    [SerializeField] protected float gravityScale;
+    [SerializeField] protected float boxCastThickness;
+    [SerializeField] protected List<Collider2D> hitColliders;
+    [SerializeField] protected float padding;
+    [SerializeField] protected float rayMaxDistance;
+    [SerializeField] protected float groundOffset;
+    [SerializeField] protected float sideOffset;
+    protected BoxCollider2D colliderSelf;
+    private RaycastHit2D[] groundColliders;
+
+    private Vector2 TopBottomBoundSize => new Vector2(colliderSelf.size.x, boxCastThickness);
+    private Vector2 LeftRightBoundSize => new Vector2(boxCastThickness, colliderSelf.size.y);
+    private Vector2 boxCastRightOrigin => 
+        new Vector2(transform.position.x + (colliderSelf.size.x + boxCastThickness) / 2 + padding - sideOffset, 
+            transform.position.y + colliderSelf.offset.y);
+    private Vector2 boxCastLeftOrigin =>
+        new Vector2(transform.position.x - (colliderSelf.size.x + boxCastThickness) / 2 - padding + sideOffset, 
+            transform.position.y + colliderSelf.offset.y);
+    private Vector2 boxCastUpOrigin =>
+        new Vector2(transform.position.x, 
+            transform.position.y + (colliderSelf.size.y + boxCastThickness) / 2 + colliderSelf.offset.y + padding);
+    private Vector2 boxCastDownOrigin =>
+        new Vector2(transform.position.x, 
+            transform.position.y - (colliderSelf.size.y + boxCastThickness) / 2 + colliderSelf.offset.y - padding + groundOffset);
+    
+    protected virtual void Init()
+    {
+        colliderSelf = this.transform.GetComponent<BoxCollider2D>();
+        hitColliders = new List<Collider2D>();
+    }
+
+    /// <summary>
+    /// 상하좌우 각각 boxcast hit 체크, 충돌하는게 있는지 체크
+    /// </summary>
+    protected void HitColliderCheck()
+    {
+        hitColliders.AddRange(Physics2D.OverlapBoxAll(boxCastUpOrigin, TopBottomBoundSize, 0));
+        hitColliders.AddRange(Physics2D.OverlapBoxAll(boxCastDownOrigin, TopBottomBoundSize, 0));
+        hitColliders.AddRange(Physics2D.OverlapBoxAll(boxCastLeftOrigin, LeftRightBoundSize, 0));
+        hitColliders.AddRange(Physics2D.OverlapBoxAll(boxCastRightOrigin, LeftRightBoundSize, 0));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        
+        var temp = Physics2D.BoxCastAll(boxCastDownOrigin, TopBottomBoundSize, 
+            0, Vector2.down, rayMaxDistance,1 << LayerMask.NameToLayer("Ground"));
+        if (temp.Length > 0)
+        {
+            foreach (var t in temp)
+            {
+                if (t.distance == 0)
+                    continue;
+                
+                Gizmos.DrawRay(boxCastDownOrigin, Vector3.down * t.distance);
+                Gizmos.DrawWireCube(boxCastDownOrigin + Vector2.down * t.distance, TopBottomBoundSize);
+            }
+        }
+        else
+        {
+            Gizmos.DrawRay(boxCastDownOrigin, Vector3.down * rayMaxDistance);
+        }
+        Gizmos.DrawWireCube(boxCastUpOrigin, TopBottomBoundSize);
+        // Gizmos.DrawWireCube(boxCastDownOrigin, TopBottomBoundSize);
+        Gizmos.DrawWireCube(boxCastLeftOrigin, LeftRightBoundSize);
+        Gizmos.DrawWireCube(boxCastRightOrigin, LeftRightBoundSize);
+    }
+
+    /// <summary>
+    /// Ground Check
+    /// </summary>
+    /// <returns>true : ground, false : midair</returns>
+    protected bool GroundCheck()
+    {
+        groundColliders = Physics2D.BoxCastAll(boxCastDownOrigin, TopBottomBoundSize, 
+            0, Vector2.down, rayMaxDistance,1 << LayerMask.NameToLayer("Ground"));
+
+        float minDistance = rayMaxDistance;
+        if (groundColliders.Length > 1)
+        {
+            minDistance = GetMinDistance(groundColliders);
+
+            this.transform.position = new Vector2(this.transform.position.x, 
+                this.transform.position.y + (colliderSelf.size.y / 2 - minDistance));
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    private float GetMinDistance(RaycastHit2D[] hit2Ds)
+    {
+        float minDist = rayMaxDistance;
+        foreach (var t in hit2Ds)
+        {
+            if (t.distance != 0)
+            {
+                if (minDist > t.distance)
+                    minDist = t.distance;
+            }
+        }
+
+        return minDist;
+    }
+
+    protected bool IsHitRight()
+    {
+        var colliders = Physics2D.BoxCastAll(boxCastRightOrigin, LeftRightBoundSize, 0, 
+            Vector2.right, rayMaxDistance, 1 << LayerMask.NameToLayer("Ground"));
+        return colliders.Length > 1 ? true : false;
+    }
+
+    protected bool IsHitLeft()
+    {
+        var colliders = Physics2D.BoxCastAll(boxCastLeftOrigin, LeftRightBoundSize, 0,
+            Vector2.left, rayMaxDistance, 1 << LayerMask.NameToLayer("Ground"));
+        return colliders.Length > 1 ? true : false;
+    }
+
+    protected bool IsHitUp(out RaycastHit2D[] colliders)
+    {
+        colliders = Physics2D.BoxCastAll(boxCastUpOrigin, TopBottomBoundSize, 0,
+            Vector2.up, rayMaxDistance);
+        return colliders.Length > 0 ? true : false;
+    }
+
+    /// <summary>
+    /// 중력 계산해서 방향 적용
+    /// Update()에서 사용
+    /// </summary>
+    protected void TranslateWithGravity()
+    {
+        if (!isGround)
+        {
+            moveDirection.y += Physics.gravity.y * Time.fixedDeltaTime * gravityScale;
+        }
+        else
+        {
+            moveDirection.y = 0;
+        }
+        
+        transform.Translate(moveDirection);
+    }
+}    
--- Assets/Script/InteractableObject.cs
+++ Assets/Script/InteractableObject.cs
@@ -3,23 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour
+public class InteractableObject : CInteractableObject
 {
+    [Header("Toybox")]
     // 기본적으로 밀기는 가능, 들수 있는지만 확인
     // InteractableObject 태그 달기
     [SerializeField] private bool isHoldableObject;
-    [SerializeField] private bool isGround;
-    [SerializeField] private Vector3 rayPosition;
-    private Vector3 calculatedRayPosRight;
-    private Vector3 calculatedRayPosLeft;
-    private Vector2 calRayRight;
-    private Vector2 calRayLeft;
-    private Vector2 calRayMiddle;
-    private Rigidbody2D rb;
+    // private Rigidbody2D rb;
     private ObjectSoundController sfx;
     private GameManager gameManager;
     private int whichCutNum;
     private bool movingXMidAir;
+    
     
     // public Player player;
     public bool instantiatedForDrag;
@@ -34,10 +29,16 @@ public class InteractableObject : MonoBehaviour
     public bool stay;
     public bool exit;
     
-    private void SetRigidbodyFreezePositionX() => 
-        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-    
-    
+    // private void SetRigidbodyFreezePositionX() => 
+    //    rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+
+    [Header("Drag")] 
+    [SerializeField] private float dragWeight;
+    [SerializeField] private bool hitSideWall;
+
+    [Header("Throw")] 
+    [SerializeField] private float throwWeight;
+
     public void Instantiated(bool flag) => this.instantiatedForDrag = flag;
     public void SyncNeeded(bool flag) => this.needSync = flag;
     public int WhichCutNum => whichCutNum;
@@ -45,111 +46,71 @@ public class InteractableObject : MonoBehaviour
     public void Init(int cutNum)
     {
         whichCutNum = cutNum;
+        base.Init();
     }
     
     private void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        // rb = this.GetComponent<Rigidbody2D>();
         sfx = this.GetComponent<ObjectSoundController>();
         objectSyncController = GameObject.Find("ObjectSyncManager").GetComponent<ObjectSyncController>();
         gameManager = GameManager.getInstance();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        isGround = GroundCheck();
-        
-        if(!(rb.velocity.y > 0 || rb.transform.localPosition.y < -4.5) && !this.stayUpperCollider)
+        if (moveDirection.y > 0)
         {
-            this.gameObject.layer = 8;
-        }
-        
-        if (this.transform.localPosition.y < -7)
-        {
-            Destroy(this.gameObject);
-        }
+            isGround = false;
 
-        // 콜라이더 충돌 체크 등 다른 방법 필요
-        if (gameManager.GetCurrentCutNum() < 3)
-        {
-            if (this.transform.localPosition.y > 5.5)
+            if (gameManager.GetCurrentCutNum() < 3)
             {
-                this.transform.localPosition =
-                    new Vector3(this.transform.localPosition.x, 5.5f, this.transform.localPosition.z);
-                rb.velocity = Vector2.zero;
+                // TODO : 나중에 CutManager 추가한 뒤 수정하기 (전체 컷의 반)
             }
         }
         else
         {
-            if (this.transform.localPosition.y > 6)
+            if (base.GroundCheck())
             {
-                Destroy(this.gameObject);
-            }
-        }
-    }
-
-    private bool GroundCheck()
-    {
-        /* // debug
-        calculatedRayPosRight = new Vector3(transform.position.x + rayPosition.x, 
-            transform.position.y + rayPosition.y, transform.position.z + rayPosition.z);
-        calculatedRayPosLeft = new Vector3(transform.position.x - rayPosition.x, 
-            transform.position.y + rayPosition.y, transform.position.z + rayPosition.z);
-        Debug.DrawRay(calculatedRayPosRight, Vector3.down * 0.1f, Color.cyan, 1);
-        Debug.DrawRay(calculatedRayPosLeft, Vector3.down * 0.1f, Color.cyan, 1);
-        */
-        
-        calRayRight = new Vector2(transform.position.x + rayPosition.x, transform.position.y + rayPosition.y);
-        calRayLeft = new Vector2(transform.position.x - rayPosition.x, transform.position.y + rayPosition.y);
-        calRayMiddle = new Vector2(transform.position.x, transform.position.y + rayPosition.y);
-
-        var hitRight = Physics2D.Raycast(calRayRight, Vector2.down, 0.1f);
-        var hitLeft = Physics2D.Raycast(calRayLeft, Vector2.down, 0.1f);
-        var hitMiddle = Physics2D.Raycast(calRayMiddle, Vector2.down, 0.1f);
-
-        var isHitLeft = false;
-        var isHitMiddle = false;
-
-        
-        if (GroundColliderCheck(hitRight, out var isHitRight) || GroundColliderCheck(hitLeft, out isHitLeft) ||
-            GroundColliderCheck(hitMiddle, out isHitMiddle))
-        {
-            if (isHitRight || isHitLeft || isHitMiddle)
-            {
-                movingXMidAir = false;
-                return true;
+                // ground
+                if (!isGround)
+                {
+                    // 이전 프레임에서 midair 였을 때
+                    moveDirection = new Vector2(0, moveDirection.y);
+                }
+                isGround = true;
             }
             else
             {
-                if (!movingXMidAir)
-                    SetRigidbodyFreezePositionX();
-                return false;
+                // midair
+                isGround = false;
+            }
+
+            if (this.transform.localPosition.y < -7)
+            {
+                Destroy(this.gameObject);
+            }
+
+            // 콜라이더 충돌 체크 등 다른 방법 필요
+            if (gameManager.GetCurrentCutNum() < 3)
+            {
+                if (this.transform.localPosition.y > 5.5)
+                {
+                    this.transform.localPosition =
+                        new Vector3(this.transform.localPosition.x, 5.5f, this.transform.localPosition.z);
+                    // rb.velocity = Vector2.zero;
+                }
+            }
+            else
+            {
+                if (this.transform.localPosition.y > 6)
+                {
+                    Destroy(this.gameObject);
+                }
             }
         }
-        else
-        {
-            if (!movingXMidAir)
-                SetRigidbodyFreezePositionX();
-            return false;
-        }
-    }
 
-    private bool GroundColliderCheck(RaycastHit2D hit, out bool isHitGround)
-    {
-        if (hit.collider == null)
-        {
-            isHitGround = false;
-            return false;
-        }
-
-        if (hit.collider.CompareTag("Throw") || hit.collider.CompareTag("Player"))
-        {
-            isHitGround = false;
-            return false;
-        }
-        
-        isHitGround = hit.collider.IsTouchingLayers(1 << LayerMask.NameToLayer("Ground"));
-        return true;
+        base.TranslateWithGravity();
     }
 
     public bool Drag(float axis, float speed)
@@ -159,9 +120,26 @@ public class InteractableObject : MonoBehaviour
             return false;
         }
 
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        rb.velocity = new Vector2(speed * axis, rb.velocity.y);
-
+        if (axis == 0)
+        {
+            moveDirection = new Vector2(0, moveDirection.y);
+        }
+        else
+        {
+            // 좌우에 벽 붙어 있으면 못 밀게
+            if (base.IsHitLeft() || base.IsHitRight())
+            {
+                hitSideWall = true;
+                moveDirection = new Vector2(0, moveDirection.y);
+                return false;
+            }
+            else
+            {
+                hitSideWall = false;
+                moveDirection = new Vector2(speed * axis / dragWeight, moveDirection.y);
+            }
+        }
+        
         if (whichCutNum == gameManager.GetCurrentCutNum())
         {
             if (axis != 0)
@@ -171,7 +149,7 @@ public class InteractableObject : MonoBehaviour
 
             if (this.needSync)
             {
-                objectSyncController.SyncObject(this.childObjectPair, rb.velocity);
+                // objectSyncController.SyncObject(this.childObjectPair, rb.velocity);
             }
         }
 
@@ -185,24 +163,23 @@ public class InteractableObject : MonoBehaviour
             return false;
         }
         
-        rb.velocity = new Vector2(0, throwPower);
-        this.gameObject.layer = 31;
+        moveDirection = new Vector2(0, throwPower / throwWeight);
         return true;
     }
 
     public void ChangeVelocity(Vector2 vel)
     {
         movingXMidAir = true;
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        this.rb.velocity = vel;
+       // rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        // this.rb.velocity = vel;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            if (collision.transform.GetComponent<Player>().GetPlayerState() == Player.PlayerState.Jump)
-                rb.velocity = Vector2.zero;
+            // if (collision.transform.GetComponent<Player>().GetPlayerState() == Player.PlayerState.Jump)
+                // rb.velocity = Vector2.zero;
         }
     }
 
@@ -227,14 +204,14 @@ public class InteractableObject : MonoBehaviour
             // 현재 벡터값 저장, 위에 스폰, 다른데 생성 (던지기)
             if (!collision.gameObject.GetComponent<BoundaryCollider>().verticalBoundary)
             {
-                objectSyncController.Thrown(this.gameObject, rb.velocity);
+                // objectSyncController.Thrown(this.gameObject, rb.velocity);
             }
             // 밀기
             else
             {
                 if (!this.instantiatedForDrag)
                 {
-                    objectSyncController.InstantiateObjects(this.gameObject, rb.velocity);
+                    // objectSyncController.InstantiateObjects(this.gameObject, rb.velocity);
                     this.instantiatedForDrag = true;
                 }
             }
@@ -251,8 +228,8 @@ public class InteractableObject : MonoBehaviour
 
         if (collision.transform.CompareTag("Player"))
         {
-            if (collision.transform.GetComponent<Player>().GetPlayerState() == Player.PlayerState.Jump)
-                rb.velocity = Vector2.zero;
+           // if (collision.transform.GetComponent<Player>().GetPlayerState() == Player.PlayerState.Jump)
+                // rb.velocity = Vector2.zero;
         }
     }
 
