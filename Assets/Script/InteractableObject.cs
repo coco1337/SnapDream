57721069ef3fb5e7004ccf83477a5caad56cf4dd
coco1337 <scar3517@gmail.com>
2020-06-21 13:47:57 +0900

    Update InteractableObject.cs
    필요없는 using 삭제

--- Assets/Script/InteractableObject.cs
+++ Assets/Script/InteractableObject.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class InteractableObject : MonoBehaviour
 {
